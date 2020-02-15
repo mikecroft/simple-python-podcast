a0c1603be38ca4d1ed785cb10692e5ac314dd9ff
Mike Croft <backtotheoldhouse@gmail.com>
2020-02-15 21:25:22 +0000

    update to use podgen instead

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 from bs4 import BeautifulSoup
 from rfeed import *
-
+from podgen import Podcast, Episode, Media
 
 def processDate(datestring):
     fields = datestring.split()
@@ -18,31 +18,48 @@ URL = 'https://www.allsaintsworcester.org.uk/talks'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 talks = soup.find_all('div', class_='sqs-audio-embed')
+
+p = Podcast(
+   name="ASW Talks",
+   description="Talks from ASW.",
+   website=URL,
+   explicit=False,
+)
 items = []
 
 for talk in talks:
     dateSpeaker = talk['data-author'].split(" - ")
     date = processDate(dateSpeaker[0])
     speaker = "None" if len(dateSpeaker) < 2 else dateSpeaker[1] 
-    url = talk['data-url']
+    if (talk['data-url'] == ""):
+        continue
+    else:
+        items.append(Episode(
+            title = talk['data-title'],
+            media = Media(
+                url = talk['data-url']
+                ),
+            summary = f'A talk from ASW by {speaker} on {date}'
+            # author = speaker,
+            # pubDate = date
+        ))
 
-    items.append(Item(
-        title = talk['data-title'],
-        link = talk['data-url'],
-        description = "",
-        author = speaker,
-        pubDate = date
-    ))
+# Add some episodes
+p.episodes = items
+# Generate the RSS feed
+rss = p.rss_str()
 
-feed = Feed(
-        title = "talks cast",
-        link = "https://www.allsaintsworcester.org.uk/talks",
-        description = "",
-        language = "en-GB",
-        lastBuildDate = datetime.datetime.now(),
-        items = items)
+print(rss)
+
+# feed = Feed(
+#         title = "talks cast",
+#         link = "https://www.allsaintsworcester.org.uk/talks",
+#         description = "",
+#         language = "en-GB",
+#         lastBuildDate = datetime.datetime.now(),
+#         items = items)
  
-print(feed.rss())
+# print(feed.rss())
 
 # https://podgen.readthedocs.io/en/latest/
 # https://podba.se/validate/#
